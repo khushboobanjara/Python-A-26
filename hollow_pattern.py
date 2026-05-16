2f5e7d52a5e2a498b7fcf12adc6c405229308469
khushboo banjara <248156347+khushboobanjara@users.noreply.github.com>
2026-05-16 10:26:03 +0530

    Add files via upload

---
+++ hollow_pattern.py
@@ -0,0 +1,99 @@
+#       *       
+#     *   *     
+#   *       *
+# * * * * * * *
+
+# n=4 
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i==4 or i==5-j:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     for j in range(2,n+1):
+#         if i==4 or j==i:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print() 
+
+#-----------------------------------------------
+
+
+# * * * * * * * 
+#   *       *
+#     *   *
+#       *
+
+# n=4
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i==1 or j==i :
+#             print("*",end=" " )
+#         else:
+#             print(" ",end=" ")
+#     for j in range(2,n+1):
+#             if i==1 or j==5-i:
+#                 print("*",end=" " )
+#             else:
+#                 print(" ",end=" ")
+#     print()
+
+
+
+#       *
+#     *   *
+#   *       *
+# * * * * * * *
+
+# n=4 
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i==4 or i==5-j:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     for j in range(2,n+1):
+#         if i==4 or j==i:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print() 
+
+
+#       *       
+#     *   *     
+#   *       *
+# *           *
+# *           *
+#   *       *
+#     *   *
+#       *
+
+# n=4 
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i==5-j:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     for j in range(2,n+1):
+#         if j==i:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print() 
+
+# n=4
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if j==i:
+#             print("*",end=" " )
+#         else:
+#             print(" ",end=" ")
+#     for j in range(2,n+1):
+#             if  j==5-i:
+#                 print("*",end=" " )
+#             else:
+#                 print(" ",end=" ")
+#     print()
